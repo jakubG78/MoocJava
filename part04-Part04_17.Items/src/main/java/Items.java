ab96658a89240bfdffe2d0c2c4d31eb752f3609c
jakubG78 <jakub.gora@wp.pl>
2021-02-06 23:40:13 +0100

    commit for p4.17&18

--- part04-Part04_17.Items/src/main/java/Items.java
+++ part04-Part04_17.Items/src/main/java/Items.java
@@ -5,10 +5,20 @@ import java.util.Scanner;
 public class Items {
 
     public static void main(String[] args) {
-        // implement here your program that uses the class Item
 
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            if (name.equals("")) {
+                break;
+            }
+            Item item = new Item(name);
+            items.add(item);
+        }
+        for (Item item : items) {
+            System.out.println(item);
+        }
     }
 }
--- part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -5,10 +5,28 @@ import java.util.Scanner;
 public class PersonalInformationCollection {
 
     public static void main(String[] args) {
-        // implement here your program that uses the PersonalInformation class
 
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("First name: ");
+            String firstName = scanner.nextLine();
+            if (firstName.equals("")) {
+                System.out.println("");
+                break;
+            }
+            System.out.print("Last name: ");
+            String lastName = scanner.nextLine();
+
+            System.out.print("Identification number: ");
+            String identificationNumber = scanner.nextLine();
+
+            infoCollection.add(new PersonalInformation(firstName, lastName, identificationNumber));
+        }
+        for (PersonalInformation personalInformation : infoCollection) {
+            System.out.println(personalInformation.getFirstName() + " " + personalInformation.getLastName());
+        }
 
     }
 }
