5da5acc13c8d04fa243f079bd28abf46a4703a87
jakubG78 <jakub.gora@wp.pl>
2021-02-06 21:14:14 +0100

    commit for p4.15

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -5,12 +5,28 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // you can write test code here
-        // however, remove all unnecessary code when doing the final parts of the exercise
+        Statistics statistics = new Statistics();
+        Statistics evenStats = new Statistics();
+        Statistics oddStats = new Statistics();
 
-        // In order for the tests to work, the objects must be created in the
-        // correct order in the main program. First the object that tracks the total
-        // sum, secondly the object that tracks the sum of even numbers, 
-        // and lastly the one that tracks the sum of odd numbers!
+        System.out.println("Enter numbers:");
+
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == -1) {
+                break;
+            }
+            statistics.addNumber(number);
+
+            if (number % 2 == 0) {
+                evenStats.addNumber(number);
+            } else {
+                oddStats.addNumber(number);
+            }
+        }
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + evenStats.sum());
+        System.out.println("Sum of odd numbers: " + oddStats.sum());
     }
 }
