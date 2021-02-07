4366f11ff8ec6c9cf136ea3bf4e4af53981f705a
jakubG78 <jakub.gora@wp.pl>
2021-02-07 12:23:21 +0100

    commit for p4.25

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -6,6 +6,14 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String inputFileName = scanner.nextLine();
+        try ( Scanner fileScanner = new Scanner(Paths.get(inputFileName))) {
+            while (fileScanner.hasNextLine()) {
+                String fileRow = fileScanner.nextLine();
+                System.out.println(fileRow);
+            }
+        } catch (Exception e) {
+            System.out.println("File reagin error: " + e.getMessage());
+        }
     }
 }
