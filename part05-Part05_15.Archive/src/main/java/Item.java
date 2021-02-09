81200153c08f6e50bda1dcbe8b8dbee505f360eb
jakubG78 <jakub.gora@wp.pl>
2021-02-09 20:23:32 +0100

    commit for p5.12

---
+++ part05-Part05_15.Archive/src/main/java/Item.java
@@ -0,0 +1,16 @@
+
+public class Item {
+
+    private String id;
+    private String name;
+
+    public Item(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+      
+    @Override
+    public String toString() {
+        return "Item{" + "id=" + id + ", name=" + name + '}';
+    }
+}
