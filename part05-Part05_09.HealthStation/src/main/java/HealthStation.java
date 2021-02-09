bbc58c9545a43085e3ee557210c68e8cecc6475e
jakubG78 <jakub.gora@wp.pl>
2021-02-09 11:34:22 +0100

    commit for p5.09

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,23 @@
 
 public class HealthStation {
 
+    private int weighingsCounter;
+
+    public HealthStation() {
+        this.weighingsCounter = 0;
+    }
 
     public int weigh(Person person) {
-        // return the weight of the person passed as the parameter
-        return -1;
+        this.weighingsCounter++;
+        return person.getWeight();
+    }
+
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+
+    public int weighings() {
+        return this.weighingsCounter;
     }
 
 }
