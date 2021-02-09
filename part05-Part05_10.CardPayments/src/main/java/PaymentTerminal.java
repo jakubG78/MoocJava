1a0d8e81b16e6a684fe78aa12f5c8ed696823f3a
jakubG78 <jakub.gora@wp.pl>
2021-02-09 12:07:53 +0100

    commit for p5.10

--- part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -1,30 +1,58 @@
 
 public class PaymentTerminal {
 
-
     private double money;  // amount of cash
     private int affordableMeals; // number of sold affordable meals
     private int heartyMeals;  // number of sold hearty meals
 
-
     public PaymentTerminal() {
-        // register initially has 1000 euros of money
+        this.money = 1000.0;
+        affordableMeals = 0;
+        heartyMeals = 0;
     }
 
     public double eatAffordably(double payment) {
-        // an affordable meal costs 2.50 euros
-        // increase the amount of cash by the price of an affordable mean and return the change
-        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (payment >= 2.50) {
+            money = money + 2.50;
+            affordableMeals++;
+            return payment - 2.50;
+        } else {
+            return payment;
+        }
+    }
+
+    public boolean eatAffordably(PaymentCard card) {
+        if (card.takeMoney(2.50)) {
+            affordableMeals++;
+            return true;
+        }
+        return false;
     }
 
     public double eatHeartily(double payment) {
-        // a hearty meal costs 4.30 euros
-        // increase the amount of cash by the price of a hearty mean and return the change
-        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (payment >= 4.30) {
+            money = money + 4.30;
+            heartyMeals++;
+            return payment - 4.30;
+        } else {
+            return payment;
+        }
     }
 
+    public boolean eatHeartily(PaymentCard card) {
+        if (card.takeMoney(4.30)) {
+            heartyMeals++;
+            return true;
+        }
+        return false;
+    }
+
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        if (sum > 0) {
+            card.addMoney(sum);
+            this.money += sum;
+        }
+    }
 
     @Override
     public String toString() {
