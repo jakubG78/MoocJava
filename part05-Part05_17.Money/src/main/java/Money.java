acd734c1d74e452e97dc883a8a6e3833f7aa7bc3
jakubG78 <jakub.gora@wp.pl>
2021-02-10 11:41:52 +0100

    commit for p5.17

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -23,6 +23,32 @@ public class Money {
         return this.cents;
     }
 
+    public Money plus(Money addition) {
+        Money newMoney = new Money(this.euros
+                + addition.euros, this.cents + addition.cents);
+        return newMoney;
+    }
+
+    public boolean lessThan(Money compared) {
+        if (this.euros == compared.euros) {
+            return this.cents < compared.cents;
+        }
+        return (this.euros < compared.euros);
+    }
+
+    public Money minus(Money decreaser) {
+        int euros = this.euros - decreaser.euros;
+        int cents = this.cents - decreaser.cents;
+        if (cents < 0) {
+            cents += 100;
+            euros -= 1;
+        }
+        if (euros < 0) {
+            return new Money(0, 0);
+        }
+        return new Money(euros, cents);
+    }
+
     public String toString() {
         String zero = "";
         if (this.cents < 10) {
