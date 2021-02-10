05eb94e38bafca1c617b7e34eadf928f81a81198
jakubG78 <jakub.gora@wp.pl>
2021-02-10 16:01:46 +0100

    commit for p6.01

--- part06-Part06_01.Menu/src/main/java/Menu.java
+++ part06-Part06_01.Menu/src/main/java/Menu.java
@@ -10,7 +10,9 @@ public class Menu {
     }
 
     public void addMeal(String meal) {
-        this.meals.add(meal);
+        if (!meals.contains(meal)) {
+            this.meals.add(meal);
+        }
     }
 
     public void printMeals() {
