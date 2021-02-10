50ff597fcd93302fd66af64c81919b2796846d33
jakubG78 <jakub.gora@wp.pl>
2021-02-10 16:15:08 +0100

    commit for p6.02

---
+++ part06-Part06_02.Stack/src/main/java/Stack.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+
+public class Stack {
+
+    private ArrayList<String> stack;
+
+    public Stack() {
+        this.stack = new ArrayList<>();
+    }
+
+    public boolean isEmpty() {
+        return this.stack.isEmpty();
+    }
+
+    public void add(String value) {
+        if (!this.stack.contains(value)) {
+            this.stack.add(value);
+        }
+    }
+    
+    public ArrayList<String> values(){
+        return this.stack;
+    }
+    
+    public String take(){
+        return this.stack.remove(this.stack.size()-1);
+    }
+}
