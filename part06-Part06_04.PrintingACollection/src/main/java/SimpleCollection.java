7f9c931c12b6a3f4aafc1b8bd42a7d842bf6b19b
jakubG78 <jakub.gora@wp.pl>
2021-02-11 11:52:20 +0100

    commit for p6.04

--- part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -18,5 +18,32 @@ public class SimpleCollection {
     public ArrayList<String> getElements() {
         return this.elements;
     }
-    
+
+    @Override
+    public String toString() {
+
+        String collectionContent = "";
+        String output = "";
+
+        for (String element : elements) {
+            if (element.equals(elements.get(elements.size() - 1))) {
+                collectionContent += element;
+            } else {
+                collectionContent += element + "\n";
+            }
+        }
+
+        if (elements.isEmpty()) {
+            return "The collection " + this.name + " is empty.";
+        }
+
+        if (elements.size() == 1) {
+            return "The collection " + this.name + " has " + this.elements.size() + " element:\n"
+                    + collectionContent;
+        } else {
+            return "The collection " + this.name + " has " + this.elements.size() + " elements:\n"
+                    + collectionContent;
+        }
+    }
+
 }
