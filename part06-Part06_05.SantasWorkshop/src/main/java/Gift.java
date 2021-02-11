43861319619764a146c0ca8925f3e832fe2125ae
jakubG78 <jakub.gora@wp.pl>
2021-02-11 12:12:23 +0100

    commit for p6.05

---
+++ part06-Part06_05.SantasWorkshop/src/main/java/Gift.java
@@ -0,0 +1,29 @@
+
+public class Gift {
+
+    private String name;
+    private int weight;
+
+    public Gift() {
+        this.name = "";
+        this.weight = 0;
+    }
+
+    public Gift(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + weight + "kg)";
+    }
+}
