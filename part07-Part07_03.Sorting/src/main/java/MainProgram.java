5a10f81ae6b878df297b15d43361b8a831262974
jakubG78 <jakub.gora@wp.pl>
2021-02-15 21:59:34 +0100

    commit for p7.03

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -1,9 +1,57 @@
 
+import java.util.Arrays;
+
 public class MainProgram {
 
     public static void main(String[] args) {
-        // write your test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
+        int[] numbers = {8, 3, 7, 9, 1, 2, 4};
+        MainProgram.sort(numbers);
+    }
+
+    public static int smallest(int[] array) {
+        int smallestNumber = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (smallestNumber > array[i]) {
+                smallestNumber = array[i];
+            }
+        }
+        return smallestNumber;
+    }
+
+    public static int indexOfSmallest(int[] array) {
+        int indexOfSmallest = 0;
+        for (int i = 0; i < array.length; i++) {
+            int smallestNumber = array[indexOfSmallest];
+            if (smallestNumber > array[i]) {
+                indexOfSmallest = i;
+            }
+        }
+        return indexOfSmallest;
+    }
+
+    public static int indexOfSmallestFrom(int[] array, int startIndex) {
+        int indexOfSmallestFrom = startIndex;
+        for (int i = startIndex; i < array.length; i++) {
+            int smallestNumber = array[indexOfSmallestFrom];
+            if (smallestNumber > array[i]) {
+                indexOfSmallestFrom = i;
+            }
+        }
+        return indexOfSmallestFrom;
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int tempValue = array[index2];
+        array[index2] = array[index1];
+        array[index1] = tempValue;
+    }
+
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int indexOfSmallestFrom = MainProgram.indexOfSmallestFrom(array, i);
+            MainProgram.swap(array, indexOfSmallestFrom, i);
+            System.out.println(Arrays.toString(array));
+        }
     }
 
 }
