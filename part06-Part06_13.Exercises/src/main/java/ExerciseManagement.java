ce57eff59de0fdca562bc9d648e4d4d5c1df0172
jakubG78 <jakub.gora@wp.pl>
2021-02-15 11:50:08 +0100

    commit for p6.13

--- part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
+++ part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
@@ -1,4 +1,50 @@
 
+import java.util.ArrayList;
+
 public class ExerciseManagement {
 
+    private ArrayList<Exercise> exercises;
+
+    public ExerciseManagement() {
+        this.exercises = new ArrayList<>();
+    }
+
+    public ArrayList<String> exerciseList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (Exercise exercise : exercises) {
+            list.add(exercise.getName());
+        }
+        return list;
+    }
+
+    public void add(String exercise) {
+        this.exercises.add(new Exercise(exercise));
+    }
+
+    public void markAsCompleted(String exercise) {
+        for (Exercise exerciseEntry : exercises) {
+            if (exerciseEntry.getName().equals(exercise)) {
+                exerciseEntry.setCompleted(true);
+            }
+        }
+    }
+
+    public boolean isCompleted(String exercise) {
+        for (Exercise exerciseEntry : exercises) {
+            if (exerciseEntry.getName().equals(exercise)) {
+                return exerciseEntry.isCompleted();
+            }
+        }
+        return false;
+    }
+
+    public void remove(String exercise) {
+        Exercise exerciseToBeRemoved = new Exercise("");
+        for (Exercise exerciseEntry : exercises) {
+            if (exerciseEntry.getName().equals(exercise)) {
+                exerciseToBeRemoved = exerciseEntry;
+            }
+        }
+        this.exercises.remove(exerciseToBeRemoved);
+    }
 }
--- part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,62 @@
 
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ExerciseManagementTest {
 
+    private ExerciseManagement management;
+
+    @Before
+    public void initialize() {
+        management = new ExerciseManagement();
+    }
+
+    @Test
+    public void exerciseListEmptyAtBegining() {
+        assertEquals(0, management.exerciseList().size());
+    }
+
+    @Test
+    public void addingExerciseGrowsListByOne() {
+        management.add("Write a test");
+        assertEquals(1, management.exerciseList().size());
+    }
+
+    @Test
+    public void addedExerciseInList() {
+        management.add("Write a test");
+        assertTrue(management.exerciseList().contains("Write a test"));
+    }
+
+    @Test
+    public void exerciseCanBeMarkedAsCompleted() {
+        management.add("New exercise");
+        management.markAsCompleted("New exercise");
+        assertTrue(management.isCompleted("New exercise"));
+    }
+
+    @Test
+    public void ifNotMarkedCompletedIsNotCompleted() {
+        management.add("New exercise");
+        management.markAsCompleted("New exercise");
+        assertFalse(management.isCompleted("Other exercise"));
+    }
+
+    @Test
+    public void removingExerciseShrinkListByOne() {
+        management.add("Test exercise to be removed");
+        management.remove("Test exercise to be removed");
+        assertEquals(0, this.management.exerciseList().size());
+    }
+
+    @Test
+    public void removingRightExercise() {
+        management.add("First task");
+        management.add("Test exercise to be removed");
+        management.remove("Test exercise to be removed");
+        assertFalse(this.management.exerciseList().contains("Test exercise to be removed"));
+    }
 }
