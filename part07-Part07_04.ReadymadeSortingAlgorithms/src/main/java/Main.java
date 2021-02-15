deec2f09a6d0818c07bbe28e1ce682aaa7d136ab
jakubG78 <jakub.gora@wp.pl>
2021-02-15 22:08:26 +0100

    commit for p7.04

--- part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -1,10 +1,35 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
         // insert test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(8);
+        numbers.add(3);
+        numbers.add(7);
+        System.out.println(numbers);
+        Collections.sort(numbers);
+        System.out.println(numbers);
+    }
+
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sort(String[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sortIntegers(ArrayList<Integer> integers) {
+        Collections.sort(integers);
+    }
+
+    public static void sortStrings(ArrayList<String> strings) {
+        Collections.sort(strings);
     }
 
 }
